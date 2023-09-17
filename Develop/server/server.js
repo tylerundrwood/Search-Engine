a891692dd246ef02bc96e1297010b33e4fb20d21
Tyler Underwood <twood9600@yahoo.com>
2023-09-17 19:27:36 -0400

    added more to the server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -23,7 +23,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
